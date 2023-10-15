2deeadc82af960039514dbfc8f67e39634a8281d
Makotox0207 <xiaoxishicheng@gmail.com>
2023-10-15 15:55:13 +0900

    コンポーネントのメモ化

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState, memo } from "react";
 import { Child1 } from "./components/Child1";
 import { Child4 } from "./components/Child4";
 
-export const App = () => {
+export const App = memo(() => {
   console.log("Appレンダリング");
 
-  const [num, setNum] = useState();
+  const [num, setNum] = useState(0);
 
   const onClickButton = () => {
     setNum(num + 1);
@@ -19,4 +19,4 @@ export const App = () => {
       <Child4 />
     </>
   );
-};
+});
